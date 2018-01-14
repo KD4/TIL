b7c834cbe6d4d9744ebe8f8f81abf035ab714c08
KD4 <kangddanddan@gmail.com>
2018-01-14 16:54:28 +0900

    Update a feature to insert contact data to localstorage for saving previous data

--- React/react-fundamental/src/components/Contact.js
+++ React/react-fundamental/src/components/Contact.js
@@ -34,6 +34,21 @@ export default class Contact extends React.Component {
         this.handleEdit = this.handleEdit.bind(this);
     }
 
+    componentWillMount() {
+        const contactData = localStorage.contactData;
+        if (contactData) {
+            this.setState({
+                contactData: JSON.parse(contactData)
+            })
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (JSON.stringify(prevState.contactData) !== JSON.stringify(this.state.contactData)) {
+            localStorage.contactData = JSON.stringify(this.state.contactData);
+        }
+    }
+
     handleRemove() {
         if (this.state.selectedKey < 0) return 0;
 
